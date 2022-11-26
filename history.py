0f4ec7d74dfef43286e6bc45d6468e0fc45e807c
KINDINTI VISHWANATH REDDY <113205971+MrKVR@users.noreply.github.com>
2022-11-26 09:57:58 +0530

    Add files via upload

---
+++ history.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Fri Nov 18 09:32:10 2022)---
+runfile('C:/Users/hp/.spyder-py3/temp.py', wdir='C:/Users/hp/.spyder-py3')
+runfile('C:/Users/hp/.spyder-py3/Regression.py', wdir='C:/Users/hp/.spyder-py3')
+runfile('C:/Users/hp/.spyder-py3/Multi regression.py', wdir='C:/Users/hp/.spyder-py3')
+runfile('C:/Users/hp/.spyder-py3/Multi regression.py', wdir='C:/Users/hp/.spyder-py3')
+runfile('C:/Users/hp/.spyder-py3/Regression.py', wdir='C:/Users/hp/.spyder-py3')
+runfile('C:/Users/hp/.spyder-py3/Multi regression.py', wdir='C:/Users/hp/.spyder-py3')
+
+## ---(Sat Nov 19 03:40:38 2022)---
+runfile('C:/Users/hp/.spyder-py3/Regression.py', wdir='C:/Users/hp/.spyder-py3')
+runfile('C:/Users/hp/.spyder-py3/Multi regression.py', wdir='C:/Users/hp/.spyder-py3')
+
+## ---(Thu Nov 24 09:36:35 2022)---
+runfile('C:/Users/hp/.spyder-py3/untitled0.py', wdir='C:/Users/hp/.spyder-py3')
+n=np.arange(5,9)
+runfile('C:/Users/hp/.spyder-py3/untitled0.py', wdir='C:/Users/hp/.spyder-py3')
+runfile('C:/Users/hp/.spyder-py3/K-nn graph.py', wdir='C:/Users/hp/.spyder-py3')
+runfile('C:/Users/hp/.spyder-py3/K-nn.py', wdir='C:/Users/hp/.spyder-py3')
+runfile('C:/Users/hp/.spyder-py3/K-nn graph.py', wdir='C:/Users/hp/.spyder-py3')
